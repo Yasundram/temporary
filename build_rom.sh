5a2bf3418dc0a3a22023040f1acd9de459898b9b
Debayan <kardebayan3@gmail.com>
2021-05-22 17:43:59 +0530

    CPH1859: Change local manifest branch
    Signed-off-by: Debayan <kardebayan3@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,7 @@
 # sync rom
 repo init --depth=1 -u https://github.com/StagOS/manifest.git -b r11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/kardebayan/local_manifests.git --depth 1 -b r11 .repo/local_manifests
+git clone https://github.com/kardebayan/local_manifests.git --depth 1 -b prebuilt-vendor .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# Patch
-cd system/core && curl -LO https://github.com/HentaiOS-legacy/platform_system_core/commit/51b87d94442fadd886cef02bee1238bc8ea6ea84.patch && git am *.patch && cd ../../
 
 # build rom
 source build/envsetup.sh
