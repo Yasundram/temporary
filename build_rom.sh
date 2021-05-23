e73a0cbf0fa5bbab94ce619c84269ec905e7371a
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-23 17:26:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,12 +3,19 @@ repo init -u https://github.com/Palladium-OS/platform_manifest.git -b 11 --depth
 git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# patches
+cd external/selinux
+curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+patch -p1 < *.patch
+cd ../..
+
 # build rom      
 source build/envsetup.sh
+export USE_GAPPS = true
 lunch palladium_RMX1941-userdebug
 mka palladium
       
 # upload rom
-rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P     
    
       
