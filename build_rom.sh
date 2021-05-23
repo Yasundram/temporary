c2c86357fed6e8583e984c80a19ae6ea2e067b54
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-05-23 19:24:19 +0530

    Test for theme picker

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom 
-repo init --depth=1 -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Shazu-xD/test1.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch fluid_RMX1801-userdebug
-mka bacon 
+brunch RMX1801
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/RMX1801/Fluid*.zip cirrus:RMX1801 -P
+rclone copy out/target/product/RMX1801/Wave*.zip cirrus:RMX1801 -P
