c7f05034868902e52e765a9e77cfb35a816bbccd
ItsVixano <vixanonew@gmail.com>
2021-05-24 10:51:45 +0200

    setup for daisy
    Change-Id: I070ffd71392c173d23b38c3a5efdecc7d41e4ff4

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,18 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ItsVixano/local_manifest --depth 1 -b daisy-pie .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# Install imagemagick
+sudo apt update -y && sudo apt install imagemagick python bc bsdmainutils -y
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch lineage_daisy-userdebug
+croot
+brunch daisy
 #if you are a patch user (which is really not normal and not recommended), then must put like this, `m aex || repo forall -c 'git checkout .'
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/product/daisy/lineage-16.0*.zip cirrus:daisy -P
