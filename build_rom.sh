0c0a2b88879360f39115fd4c201abbee2d0f1b85
ItsVixano <vixanonew@gmail.com>
2021-05-24 11:20:53 +0200

    update
    Change-Id: Ie605bf54ed61305d9f2fd42e9baebcdec7d07805

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
+# Install imagemagick
+sudo apt update -y && sudo apt install imagemagick python bc bsdmainutils -y
+
 # sync rom
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ItsVixano/local_manifest --depth 1 -b daisy-pie .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# Install imagemagick
-sudo apt update -y && sudo apt install imagemagick python bc bsdmainutils -y
 
 # build rom
 source build/envsetup.sh
