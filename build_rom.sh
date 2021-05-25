f73608802350105d78d35b403a62129d9bc31a0c
BOND007 <mhashimkp@gmail.com>
2021-05-25 11:22:35 +0530

    Xtended

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Project-Xtended/manifest.git -b xr -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Hashimkp/local_manifests.git --depth 1 -b aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch havoc_RMX1971-userdebug
-mka bacon -j$(nproc --all)
+lunch xtended_RMX1971-userdebug
+make xtended -j$(nproc --all)
 
 # upload rom
-rclone copy out/target/product/RMX1971/Havoc-OS*.zip cirrus:RMX1971 -P
+rclone copy out/target/product/RMX1971/Xtended*.zip cirrus:RMX1971 -P
