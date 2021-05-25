b58416db024f8a77271162824893a94c57fc2315
Rishawn Iyer <70256146+Stealth1226@users.noreply.github.com>
2021-05-25 21:22:38 +0530

    Lighthouse raphael  build

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Stealth1226/local_manifest --depth=1 -b raphael .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+mkdir hardware/qcom/display/config/
+cp -ar hardware/qcom-caf/sm8150/display/config/*.xml hardware/qcom/display/config/
+brunch raphael
 #if you are a patch user (which is really not normal and not recommended), then must put like this, " m aex || repo forall -c 'git checkout .' "
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/product/raphael/*OFFICIAL*.zip raphael:mido -P
