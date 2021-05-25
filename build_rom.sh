3669615da1918cb104a3e13e8d63bcacc63f4291
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-05-25 16:36:15 +0700

    TEST DT

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,27 @@ git clone https://github.com/Fraschze97/local_manifest --depth=1 -b nusantara .r
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# patches
+# Patches
 cd external/selinux
 curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
 patch -p1 < *.patch
 cd ../..
+
+cd frameworks/base
+curl -LO https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
+patch -p1 < *.patch
+cd ../..
+
+cd frameworks/opt/net/wifi
+curl -LO https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
+patch -p1 < *.patch
+cd ../../../..
+
+cd media/libstagefright
+curl -LO https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch
+patch -p1 < *.patch
+cd ../../../..
+
 
 # build
 . build/envsetup.sh
