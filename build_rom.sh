7bed7157b6b2f6b046b2aecb2638275cbe5498a4
P-Salik <peersalik16@gmail.com>
2021-05-26 15:57:27 +0530

    PixelExperience RMX1941

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 
 # sync rom
-repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Patches
-#cd media/libstagefright
-#curl -LO https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch
-#patch -p1 < *.patch
-#cd ../../../..
+cd frameworks/av
+curl -LO https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch
+patch -p1 < *.patch
+cd ../../../..
 
 # build
 . build/envsetup.sh
