26add0baf4c6207f80718cc8c75374cdbab5c1bd
Sarthak <sarthakroy2002@gmail.com>
2021-05-26 14:16:17 +0530

    RMUI 2 BASED LOS 18.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
+git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2050-userdebug
+lunch lineage_RMX2020-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 mka bacon -j$(nproc --all)
 
 # upload rom
-rclone copy out/target/product/RMX2050/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/RMX2020/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
 
