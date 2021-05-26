1e4fb65f963e5ffc6b9dbe86caf2dbea24c47b99
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-05-26 07:40:21 +0530

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -11,5 +11,6 @@ lunch ssos_violet-userdebug
 brunch violet
 
 # upload rom
+# 1
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/violet/ShapeShiftOS*.zip cirrus:violet -P
