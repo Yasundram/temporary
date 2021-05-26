91ab6cbcc20005d4e95ff113b8e3b497f8234ea3
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-05-26 20:33:39 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,18 @@ git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/loca
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Patches
-cd frameworks/av
+cd external/selinux
+curl -LO  https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+patch -p1 < *.patch
+cd ../..
+
+cd platform/frameworks/av
 curl -LO https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch
+patch -p1 < *.patch
+cd ../../../..
+
+cd frameworks/opt/net/ims
+curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0ah.patch
 patch -p1 < *.patch
 cd ../../../..
 
